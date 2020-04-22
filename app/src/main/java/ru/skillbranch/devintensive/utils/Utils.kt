24729c3e1e7c4a95f066d5cb8990bb26bba62a11
dmitrii <onolite@gmail.com>
2020-04-22 11:26:38 +0000

    Homework

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,4 +1,9 @@
 package ru.skillbranch.devintensive.utils
 
 class Utils {
+    fun parseFullName(fullName: String): Pair<String, String> {
+        val firstName = fullName?.substringBefore(' ')
+        val lastName = fullName?.substringAfter(' ')
+        return Pair(firstName, lastName)
+    }
 }
